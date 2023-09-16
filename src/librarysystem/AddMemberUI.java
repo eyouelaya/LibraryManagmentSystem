dad6061b390904b63545aebc61df3372e9a2ebcc
lex <emmy007.el@gmail.com>
2023-09-16 10:27:22 -0500

    fix empty validation

--- src/librarysystem/AddMemberUI.java
+++ src/librarysystem/AddMemberUI.java
@@ -110,6 +110,7 @@ public class AddMemberUI extends JFrame {
         mainPanel.add(bottomPanel);
 
         add(mainPanel);
+        toaster = new Toaster(mainPanel);
 
         inputPanel.setBackground(UIUtils.COLOR_BACKGROUND);
         topPanel.setBackground(UIUtils.COLOR_BACKGROUND);
