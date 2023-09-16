93aa63df8b768fbbe2392b8b2e46f6426ce1cb4f
Eyouel <eyouelaya@gmail.com>
2023-09-15 23:48:56 -0500

    FIX UI for Checkout

--- src/librarysystem/CheckOutUIForm.java
+++ src/librarysystem/CheckOutUIForm.java
@@ -1,5 +1,7 @@
 package librarysystem;
 
+import Utils.TextFieldUsername;
+import Utils.UIUtils;
 import business.SystemController;
 
 import javax.swing.*;
@@ -13,10 +15,10 @@ public class CheckOutUIForm extends JFrame {
     private JPanel panel5;
     private JPanel panel3;
     private JLabel label3;
-    private JTextField memberIDTextField;
+    private TextFieldUsername memberIDTextField;
     private JPanel panel2;
     private JLabel label2;
-    private JTextField isbnTextField;
+    private TextFieldUsername isbnTextField;
     public CheckOutUIForm() {
         initComponents();
     }
@@ -30,10 +32,10 @@ public class CheckOutUIForm extends JFrame {
         panel5 = new JPanel();
         panel3 = new JPanel();
         label3 = new JLabel();
-        memberIDTextField = new JTextField();
+        memberIDTextField = new TextFieldUsername();
         panel2 = new JPanel();
         label2 = new JLabel();
-        isbnTextField = new JTextField();
+        isbnTextField = new TextFieldUsername();
 
         //======== this ========
         setLayout(new BorderLayout(8, 8));
@@ -45,13 +47,22 @@ public class CheckOutUIForm extends JFrame {
             //---- checkoutButton ----
             checkoutButton.setText("Checkout book");
             panel4.add(checkoutButton);
+            panel4.setBackground(UIUtils.COLOR_BACKGROUND);
         }
         add(panel4, BorderLayout.PAGE_END);
 
         //---- label1 ----
+        JPanel titlePanel = new JPanel();
         label1.setText("Checkout book");
+        label1.setFont(UIUtils.FONT_GENERAL_UI);
+        titlePanel.setBackground(UIUtils.COLOR_BACKGROUND);
+        titlePanel.add(label1);
+        label1.setForeground(Color.white);
+        label1.setBackground(UIUtils.COLOR_BACKGROUND);
         label1.setHorizontalAlignment(SwingConstants.CENTER);
-        add(label1, BorderLayout.PAGE_START);
+        add(titlePanel, BorderLayout.PAGE_START);
+        setBackground(UIUtils.COLOR_BACKGROUND);
+        checkoutButton.setBackground(UIUtils.COLOR_INTERACTIVE_DARKER);
 
         //======== panel5 ========
         {
@@ -66,11 +77,15 @@ public class CheckOutUIForm extends JFrame {
 
                 //---- label3 ----
                 label3.setText("Member ID: ");
+                label3.setForeground(Color.white);
+                label3.setFont(UIUtils.FONT_GENERAL_UI);
                 panel3.add(label3);
+
 
                 //---- memberIDTextField ----
                 memberIDTextField.setColumns(20);
                 panel3.add(memberIDTextField);
+                panel3.setBackground(UIUtils.COLOR_BACKGROUND);
             }
             panel5.add(panel3);
 
@@ -80,15 +95,22 @@ public class CheckOutUIForm extends JFrame {
 
                 //---- label2 ----
                 label2.setText("Book ISBN: ");
+                label2.setForeground(Color.white);
+                label2.setFont(UIUtils.FONT_GENERAL_UI);
                 panel2.add(label2);
+                panel2.setBackground(UIUtils.COLOR_BACKGROUND);
 
                 //---- isbnTextField ----
                 isbnTextField.setColumns(20);
                 panel2.add(isbnTextField);
+                panel2.setBackground(UIUtils.COLOR_BACKGROUND);
             }
             panel5.add(panel2);
         }
         add(panel5, BorderLayout.CENTER);
+        panel5.setBackground(UIUtils.COLOR_BACKGROUND);
+
+        setBackground(UIUtils.COLOR_BACKGROUND);
         setSize(GuiProperties.SCREEN_WIDTH, GuiProperties.SCREEN_HEIGHT);
         GuiProperties.centerFrameOnDesktop(this);
 
@@ -105,6 +127,8 @@ public class CheckOutUIForm extends JFrame {
             } else JOptionPane.showMessageDialog(CheckOutUIForm.this, "Please enter details.");
         });
     }
+
+
 
     public void displayBookUnavailable() {
         JOptionPane.showMessageDialog(this, "Book not available");
