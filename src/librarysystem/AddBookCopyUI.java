026af3117002cc3d331db8c06cf2d2f557e89e8e
Eyouel <eyouelaya@gmail.com>
2023-09-16 00:17:20 -0500

    FIX UI for Add Book Copy

--- src/librarysystem/AddBookCopyUI.java
+++ src/librarysystem/AddBookCopyUI.java
@@ -1,6 +1,8 @@
 package librarysystem;
 
 import Toaster.Toaster;
+import Utils.TextFieldUsername;
+import Utils.UIUtils;
 import business.SystemController;
 
 import javax.swing.*;
@@ -14,9 +16,9 @@ public class AddBookCopyUI extends JFrame {
     private JPanel mainPanel;
     private JPanel inputPanel;
     private JLabel copyNumberLabel;
-    private JTextField copyNumberTextField;
+    private TextFieldUsername copyNumberTextField;
     private JLabel isbnLabel;
-    private JTextField isbnTextField;
+    private TextFieldUsername isbnTextField;
     private JButton addButton;
 
     public AddBookCopyUI() {
@@ -27,46 +29,60 @@ public class AddBookCopyUI extends JFrame {
     private void initComponents() {
         setTitle("Add Book Copy");
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
         setSize(400, 200);
         setLocationRelativeTo(null);
         setResizable(false);
 
         // Title Label
         titleLabel = new JLabel("Add Book Copy");
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        titleLabel.setFont(UIUtils.FONT_GENERAL_UI);
+        titleLabel.setForeground(Color.white);
         titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
         // Main Panel
-        mainPanel = new JPanel(new BorderLayout());
+        mainPanel = new JPanel(new GridLayout(3,1,10,10));
         toaster = new Toaster(mainPanel);
         mainPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
         // Input Panel
-        inputPanel = new JPanel(new GridLayout(2, 2, 10, 10));
+        inputPanel = new JPanel(new GridLayout(3, 2, 10, 10));
 
         // Copy Number Input
         copyNumberLabel = new JLabel("Copy Number:");
-        copyNumberTextField = new JTextField(10);
+        copyNumberLabel.setFont(UIUtils.FONT_GENERAL_UI);
+        copyNumberLabel.setForeground(Color.white);
+        copyNumberTextField = new TextFieldUsername();
+        copyNumberTextField.setColumns(10);
 
         // ISBN Input
         isbnLabel = new JLabel("ISBN:");
-        isbnTextField = new JTextField(10);
+        isbnLabel.setFont(UIUtils.FONT_GENERAL_UI);
+        isbnLabel.setForeground(Color.white);
+        isbnTextField = new TextFieldUsername();
+        isbnTextField.setColumns(10);
+        // Add Button
+        addButton = new JButton("Add Copy");
+        addButton.setBackground(new Color(0, 128, 0));
+        addButton.setForeground(Color.WHITE);
+
+        JLabel x= new  JLabel("x");
+        x.setForeground(UIUtils.COLOR_BACKGROUND);
 
         inputPanel.add(copyNumberLabel);
         inputPanel.add(copyNumberTextField);
         inputPanel.add(isbnLabel);
         inputPanel.add(isbnTextField);
+        inputPanel.add(x);
+        inputPanel.add(addButton);
+        inputPanel.setBackground(UIUtils.COLOR_BACKGROUND);
 
-        // Add Button
-        addButton = new JButton("Add Copy");
-        addButton.setFont(new Font("Arial", Font.PLAIN, 16));
-        addButton.setBackground(new Color(0, 128, 0));
-        addButton.setForeground(Color.WHITE);
+
 
         // Add components to main panel
         mainPanel.add(titleLabel, BorderLayout.NORTH);
         mainPanel.add(inputPanel, BorderLayout.CENTER);
-        mainPanel.add(addButton, BorderLayout.SOUTH);
+        mainPanel.setBackground(UIUtils.COLOR_BACKGROUND);
 
         // Add main panel to the frame
         add(mainPanel);
